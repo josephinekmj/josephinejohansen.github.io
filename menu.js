bc412c2811fd4ef2bcf7e056be23189a6a0a4e84
josephinekmj <josephinejohansen@pm.me>
2025-09-20 11:10:52 +0200

    deleted 2 pages

--- menu.js
+++ menu.js
@@ -1,8 +1,6 @@
         window.SITE_MENU = [
         {href: "index.html", label: "Home" },
         {href: "about.html", label: "About" },
-        {href: "portfolio.html", label: "Portfolio" },
-        {href: "4thsemesterproduct.html", label: "4. semester produkt"},
         {href: "contact.html", label: "Contact" },
         {href: "dailyaffirmations.html", label: "Affirmations" }
         
